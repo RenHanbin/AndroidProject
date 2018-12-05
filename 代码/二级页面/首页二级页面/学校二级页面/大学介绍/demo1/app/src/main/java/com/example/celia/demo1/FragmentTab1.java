028c948e1728876e6b7433b71ade1d11b6380dbc
xingjiayu <2201667098@qq.com>
2018-12-05 16:40:22 +0800

    大学介绍

---
+++ 代码/二级页面/首页二级页面/学校二级页面/大学介绍/demo1/app/src/main/java/com/example/celia/demo1/FragmentTab1.java
@@ -0,0 +1,64 @@
+package com.example.celia.demo1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class FragmentTab1 extends Fragment{
+    //创建View时调用
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page1,container,false);//根据布局文件产生布局控件
+        //false 不放到第二个参数中，true 放到第二个参数中
+        ImageView ivhot=view.findViewById(R.id.iv_hot);
+        ivhot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(getActivity().getApplicationContext(),HotMajorActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView ivschool=view.findViewById(R.id.iv_school);
+        ivschool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(getActivity().getApplicationContext(),UniversityIntroduce.class);
+                startActivity(intent);
+            }
+        });
+        ImageView ivarea=view.findViewById(R.id.iv_area);
+        ivarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(getActivity().getApplicationContext(),RegionalProfile.class);
+                startActivity(intent);
+            }
+        });
+        ImageView iveco=view.findViewById(R.id.iv_eco);
+        iveco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(getActivity().getApplicationContext(),RegionalEconomy.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+    //当view创建完成
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
