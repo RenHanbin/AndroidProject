4207a01873d25287e8c2c0df1a6a962281bf708c
邢文然 <839758676@qq.com>
2018-12-09 19:05:39 +0800

    remenzhuanyedatabase

---
+++ 代码/二级页面/首页二级页面/热门专业/连接数据库/demo1/app/src/main/java/com/example/celia/demo1/bean/Writer.java
@@ -0,0 +1,76 @@
+package com.example.celia.demo1.bean;
+
+public class Writer {
+    private int writerId;
+    private String writerName;
+    private String writerImg;
+
+
+    public Writer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public Writer(int writerId, String writerName, String writerImg) {
+        this.writerId = writerId;
+        this.writerName = writerName;
+        this.writerImg = writerImg;
+    }
+
+    public int getWriterId() {
+        return writerId;
+    }
+
+    public void setWriterId(int writerId) {
+        this.writerId = writerId;
+    }
+
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public String getWriterImg() {
+        return writerImg;
+    }
+
+    public void setWriterImg(String writerImg) {
+        this.writerImg = writerImg;
+    }
+
+    @Override
+    public String toString() {
+        return "Writer{" +
+                "writerId=" + writerId +
+                ", writerName='" + writerName + '\'' +
+                ", writerImg='" + writerImg + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Writer other = (Writer) obj;
+        if (writerId != other.writerId)
+            return false;
+        if (writerImg == null) {
+            if (other.writerImg != null)
+                return false;
+        } else if (!writerImg.equals(other.writerImg))
+            return false;
+        if (writerName == null) {
+            if (other.writerName != null)
+                return false;
+        } else if (!writerName.equals(other.writerName))
+            return false;
+        return true;
+    }
+
+}
